142f93688a18a6c12897442d6ec6859894cce234
Michal Stankiewicz <michalstankiewicz88@gmail.com>
2020-05-10 12:46:17 +0200

    setup project

---
+++ src/test/java/pl/sdaacademy/WeatherRestApi/WeatherRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.sdaacademy.WeatherRestApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
